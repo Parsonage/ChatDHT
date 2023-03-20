d87dc5fce03fc472f4fe8379a9b2640032640449
Eddie Romanov <81505200+SnoopDraugg@users.noreply.github.com>
2023-03-20 18:53:39 -0400

    Implement basic POC functionality

--- chat_dht.py
+++ chat_dht.py
@@ -3,3 +3,14 @@ import os
 import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+prompt = 'Explain art from a non-dual perspective.'
+completion = openai.ChatCompletion.create(
+    model='gpt-3.5-turbo',
+    messages=[
+        {'role': 'system', 'content': 'You are a non-dual mystic.'},
+        {'role': 'user', 'content': prompt}
+    ]
+)
+
+print(completion['choices'][0]['message']['content'])
